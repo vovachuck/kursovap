43610d8bc93c222e9d45cc1704a5d46f5592a764
VovKulaka <vv122333221vv@gmail.com>
2018-06-05 18:58:15 +0300

    beta 1.97

--- src/main/resources/public/requests/request11.js
+++ src/main/resources/public/requests/request11.js
@@ -3,7 +3,7 @@ var app = angular.module("kursova", []);
 app.controller("AppCtrl", function ($http, $scope){
     $scope.request11 = [];
 
-    $http.get('localhost:5432/request/request11').then(function (response){
+    $http.get('/request/request11').then(function (response){
         $scope.request11=response.data;
         console.log(response);
     });
